3200fb980afc04c4a85c71242e8954e8b57a0f85
Roksolana Kosiv <kosiv.roksolana@gmail.com>
2018-03-31 21:59:09 +0300

    Model.

---
+++ HourOfCode/HourOfCode/Model/Group+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Group+CoreDataClass.swift
+//  
+//
+//  Created by Roksolana Kosiv on 3/31/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Group)
+public class Group: NSManagedObject {
+
+}
---
+++ HourOfCode/HourOfCode/Model/School+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  School+CoreDataClass.swift
+//  
+//
+//  Created by Roksolana Kosiv on 3/31/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(School)
+public class School: NSManagedObject {
+
+}

727300e55c01b38cd2e454b2122feab4fb3633f1
Viktor Rudyk <rudykvictor@gmail.com>
2018-04-01 15:32:02 +0300

    added MBProgressHud pod and base methods

---
+++ HourOfCode/HourOfCode/Others/HourOfCode-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#ifndef HourOfCode_Bridging_Header_h
+#define HourOfCode_Bridging_Header_h
+
+#import <MBProgressHUD/MBProgressHUD.h>
+
+#endif
